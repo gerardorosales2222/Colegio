db036fa06c1078bfb87aea5258628a98924baf00
gerardorosales2222 <gerardorosales2222@gmail.com>
2024-08-23 15:49:46 -0300

    Primer migración

--- academico/admin.py
+++ academico/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Profesor)
+class profesor(admin.ModelAdmin):
+    list_display = ('id', 'dni', 'nombre', 'apellido', 'tel')
+"""
+@admin.register(Alumno)
+class alumno(admin.ModelAdmin):
+    list_display = ('id', 'dni', 'nombre', 'apellido', 'tel','get_materias')
+
+    def get_materias(self, obj):
+        return ', '.join([Materia.nombre for Carrera in obj.Materia.all()])
+"""
+
+admin.site.register(Materia)
+
+
+admin.site.site_header = 'Colegio Sagrado Corazón'
+admin.site.index_title = 'Panel de control'
+admin.site.site_title = 'Administración del Colegio'
--- academico/models.py
+++ academico/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Profesor(models.Model):
+    dni = models.CharField(max_length=8, null=False, verbose_name='DNI')
+    nombre = models.CharField(max_length=50, null=False, verbose_name='Nombre')
+    apellido = models.CharField(max_length=40, null=False, verbose_name='Apellido')
+    cuil = models.CharField(max_length=11, null=False, verbose_name='CUIL')
+    tel = models.CharField(max_length=10, null=False, verbose_name='Teléfono')
+    def __str__(self):
+        return '%s %s'%(self.apellido, self.nombre)  
+    class Meta:
+        db_table = 'Profesor'
+        verbose_name = 'Profesor'
+        verbose_name_plural = 'Profesores'
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=50, null=False, verbose_name='Nombre')
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE, blank=False)
+    def __str__(self):
+        return '%s '%(self.nombre)  
+    class Meta:
+        db_table = 'Materia'
+        verbose_name = 'Materia'
+        verbose_name_plural = 'Materias'
+
+""""
+class Alumno(models.Model):
+    dni = models.CharField(max_length=8, null=False, verbose_name='DNI')
+    nombre = models.CharField(max_length=20, null=False, verbose_name='Nombre')
+    apellido = models.CharField(max_length=20, null=False, verbose_name='Apellido')
+    tel = models.CharField(max_length=10, null=False, verbose_name='Teléfono')
+    materias = models.ManyToManyField(Materias, blank=False)
+    def __str__(self):
+        return '%s %s'%(self.apellido, self.nombre)  
+    class Meta:
+        db_table = 'Alumno'
+        verbose_name = 'Alumno'
+        verbose_name_plural = 'Alumnos'
+"""
