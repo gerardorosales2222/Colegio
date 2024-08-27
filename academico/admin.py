ca6d4e36162138650eaa6db22a3498d62320ce73
gerardorosales2222 <gerardorosales2222@gmail.com>
2024-08-27 20:10:54 -0300

    add tables

--- academico/admin.py
+++ academico/admin.py
@@ -1,21 +1,49 @@
 from django.contrib import admin
-from .models import *
+from .models import TituloProfesional, Profesor, Familia, Curso, Alumno, Materia, Tutor
+
+@admin.register(TituloProfesional)
+class TituloProfesionalAdmin(admin.ModelAdmin):
+    list_display = ('denominacion', 'observacion')
+    search_fields = ('denominacion',)
 
 @admin.register(Profesor)
-class profesor(admin.ModelAdmin):
-    list_display = ('id', 'dni', 'nombre', 'apellido', 'tel')
-"""
+class ProfesorAdmin(admin.ModelAdmin):
+    list_display = ('apellidos', 'nombres', 'cuil', 'tel', 'f_nacimiento', 'get_titulos')
+    search_fields = ('apellidos', 'nombres', 'cuil')
+    list_filter = ('titulos',)
+
+    def get_titulos(self, obj):
+        return ', '.join([titulos.denominacion for titulos in obj.titulos.all()])
+    get_titulos.short_description = 'títulos'    
+    
+
+@admin.register(Familia)
+class FamiliaAdmin(admin.ModelAdmin):
+    list_display = ('apellidos', 'titular')
+    search_fields = ('apellidos',)
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('año', 'color')
+    search_fields = ('año', 'color')
+
 @admin.register(Alumno)
-class alumno(admin.ModelAdmin):
-    list_display = ('id', 'dni', 'nombre', 'apellido', 'tel','get_materias')
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ('apellidos', 'nombres', 'dni', 'tel', 'familia', 'tutor', 'curso')
+    search_fields = ('apellidos', 'nombres', 'dni')
+    list_filter = ('curso',)
 
-    def get_materias(self, obj):
-        return ', '.join([Materia.nombre for Carrera in obj.Materia.all()])
-"""
+@admin.register(Materia)
+class MateriaAdmin(admin.ModelAdmin):
+    list_display = ('denominacion', 'profesor_titular', 'profesor_interino')
+    search_fields = ('denominacion',)
+    list_filter = ('profesor_titular', 'profesor_interino')
 
-admin.site.register(Materia)
-
-
+@admin.register(Tutor)
+class TutorAdmin(admin.ModelAdmin):
+    list_display = ('apellidos', 'nombres', 'dni', 'tel', 'familia', 'campo')
+    search_fields = ('apellidos', 'nombres', 'dni')
+    list_filter = ('familia',)
 admin.site.site_header = 'Colegio Sagrado Corazón'
 admin.site.index_title = 'Panel de control'
 admin.site.site_title = 'Administración del Colegio'
--- academico/models.py
+++ academico/models.py
@@ -1,39 +1,79 @@
 from django.db import models
 
-class Profesor(models.Model):
-    dni = models.CharField(max_length=8, null=False, verbose_name='DNI')
-    nombre = models.CharField(max_length=50, null=False, verbose_name='Nombre')
-    apellido = models.CharField(max_length=40, null=False, verbose_name='Apellido')
-    cuil = models.CharField(max_length=11, null=False, verbose_name='CUIL')
-    tel = models.CharField(max_length=10, null=False, verbose_name='Teléfono')
+class TituloProfesional(models.Model):
+    denominacion = models.CharField(max_length=100)
+    observacion = models.TextField(null=True)
     def __str__(self):
-        return '%s %s'%(self.apellido, self.nombre)  
+        return '%s - %s '%(self.id,self.denominacion)
     class Meta:
-        db_table = 'Profesor'
-        verbose_name = 'Profesor'
-        verbose_name_plural = 'Profesores'
+        db_table = 'titulos_profes'
+        verbose_name = 'titulo'
+        verbose_name_plural = 'titulos de profes'
+
+class Profesor(models.Model):
+    apellidos = models.CharField(max_length=100, null=True)
+    nombres = models.CharField(max_length=100, null=True)
+    cuil = models.CharField(max_length=20)
+    tel = models.CharField(max_length=20)
+    f_nacimiento = models.DateField(null=True)
+    titulos = models.ManyToManyField(TituloProfesional, blank=False)
+    def __str__(self):
+        return '%s %s '%(self.apellidos,self.nombres)
+    class Meta:
+        db_table = 'profesores'
+        verbose_name = 'profesor'
+        verbose_name_plural = 'profesores'    
+    
+class Familia(models.Model):
+    apellidos = models.CharField(max_length=100,null=True)
+    titular = models.ForeignKey(Profesor, on_delete=models.CASCADE,null=True)
+    class Meta:
+        db_table = 'familias'
+        verbose_name = 'familia'
+        verbose_name_plural = 'familias'
+    
+class Curso(models.Model):
+    año = [('1ro', 'Primer año'),('2do', 'Segundo año'),('3ro', 'Tercer año'),('4to', 'Cuarto año'),('5to', 'Quinto año'),]
+    color = [('Am', 'Amarillo'),('V', 'Verde'),('R', 'Rojo'),('Az', 'Azul'),]
+    def __str__(self):
+        return '%s %s '%(self.año,self.color)
+    class Meta:
+        db_table = 'cursos'
+        verbose_name = 'curso'
+        verbose_name_plural = 'cursos'
+
+class Alumno(models.Model):
+    apellidos = models.CharField(max_length=100, null=True)
+    nombres = models.CharField(max_length=100, null=True)
+    dni = models.CharField(max_length=20)
+    tel = models.CharField(max_length=20)
+    familia = models.ForeignKey(Familia, on_delete=models.CASCADE)
+    tutor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'alumnos'
+        verbose_name = 'alumno'
+        verbose_name_plural = 'alumnos'
 
 class Materia(models.Model):
-    nombre = models.CharField(max_length=50, null=False, verbose_name='Nombre')
-    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE, blank=False)
-    def __str__(self):
-        return '%s '%(self.nombre)  
+    denominacion = models.CharField(max_length=100, null=True)
+    profesor_titular = models.ForeignKey(Profesor, related_name='titular', null=True, on_delete=models.CASCADE)
+    profesor_interino = models.ForeignKey(Profesor, related_name='interino', null=True, on_delete=models.CASCADE)
     class Meta:
-        db_table = 'Materia'
-        verbose_name = 'Materia'
-        verbose_name_plural = 'Materias'
-
-""""
-class Alumno(models.Model):
-    dni = models.CharField(max_length=8, null=False, verbose_name='DNI')
-    nombre = models.CharField(max_length=20, null=False, verbose_name='Nombre')
-    apellido = models.CharField(max_length=20, null=False, verbose_name='Apellido')
-    tel = models.CharField(max_length=10, null=False, verbose_name='Teléfono')
-    materias = models.ManyToManyField(Materias, blank=False)
+        db_table = 'materias'
+        verbose_name = 'materia'
+        verbose_name_plural = 'materias'
+        
+class Tutor(models.Model):
+    apellidos = models.CharField(max_length=100, null=True)
+    nombres = models.CharField(max_length=100, null=True)
+    dni = models.CharField(max_length=20)
+    tel = models.CharField(max_length=20)
+    familia = models.ForeignKey(Familia, on_delete=models.CASCADE)
+    campo = models.CharField(max_length=100)
     def __str__(self):
-        return '%s %s'%(self.apellido, self.nombre)  
+        return '%s %s '%(self.apellidos,self.nombres)
     class Meta:
-        db_table = 'Alumno'
-        verbose_name = 'Alumno'
-        verbose_name_plural = 'Alumnos'
-"""
+        db_table = 'tutores'
+        verbose_name = 'tutor'
+        verbose_name_plural = 'tutores'
